e0a09093980ec3a1eaee9900d4e99901c8046652
Brian Stack <bis12@case.edu>
2013-03-21 16:43:31 -0400

    Try to work with websockets

--- dopplerganger/static/js/doppler.js
+++ dopplerganger/static/js/doppler.js
@@ -9,15 +9,14 @@ function run() {
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(.55)")
       .append("g");
 
-    svg.append("circle")
-            .attr("cx", 0)
-            .attr("cy", 0)
-            .attr("r", 2.5);
-
     var connection = new WebSocket('ws://' + window.location.host + '/dots');
 
     connection.onmessage = function(evt) {
-        console.log(evt.data);
+        var point = JSON.parse(evt.data);
+        svg.append("circle")
+                .attr("cx", point.x)
+                .attr("cy", point.y)
+                .attr("r", 2.5);
     }
 
 }
