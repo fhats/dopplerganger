b7fad751747b2aff5115bbc937027bc2736560e0
Brian Stack <bis12@case.edu>
2013-03-21 16:36:25 -0400

    Make it look a little nicer

--- dopplerganger/static/js/doppler.js
+++ dopplerganger/static/js/doppler.js
@@ -2,7 +2,7 @@ function run() {
     var width = 960;
     var height = 500;
 
-    var svg = d3.select("body").append("svg")
+    var svg = d3.select("#showbox").insert("svg", ":first-child")
         .attr("width", width)
         .attr("height", height)
       .append("g")
@@ -14,7 +14,7 @@ function run() {
             .attr("cy", 0)
             .attr("r", 2.5);
 
-    var connection = new WebSocket('ws://'+window.location.host+'/dots');
+    var connection = new WebSocket('ws://' + window.location.host + '/dots');
 
     connection.onmessage = function(evt) {
         console.log(evt.data);
