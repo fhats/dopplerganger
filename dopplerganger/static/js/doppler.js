31a1a8c81385813e0e2215b3f4a026d866800688
Brian Stack <bis12@case.edu>
2013-03-21 16:06:17 -0400

    Setup d3

---
+++ dopplerganger/static/js/doppler.js
@@ -0,0 +1,23 @@
+function run() {
+    var width = 960;
+    var height = 500;
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(.55)")
+      .append("g");
+
+    svg.append("circle")
+            .attr("cx", 0)
+            .attr("cy", 0)
+            .attr("r", 2.5);
+
+    var connection = new WebSocket('ws://'+window.location.host+'/dots');
+
+    connection.onmessage = function(evt) {
+        console.log(evt.data);
+    }
+
+}
